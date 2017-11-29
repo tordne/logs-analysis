70322049ad2844072e04c5b184fd04d87fa0350c
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2017-11-29 00:17:35 +0000

    Feat: Setup reports.py file with first function popular_article

---
+++ reports.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import psycopg2
+
+
+def popular_article():
+    '''
+    Connect to the news database and
+    extract the 3 most popular articles.
+    '''
+
+    pass
+
+
+if __name__ == '__main__':
+    # 1. What are the 3 most popular articles.
+    popular_article()
