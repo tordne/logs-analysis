a36df8a461ff262fae909c5044d045b22355e1a3
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2017-11-29 23:21:13 +0000

    Feat: Add connection, cursor and closure of db

--- reports.py
+++ reports.py
@@ -13,5 +13,13 @@ def popular_article():
 
 
 if __name__ == '__main__':
+    # Open the database and store the connection in con
+    con = psycopg2.connect("dbname=news")
+    # Assign a cursor 'c' to the connection
+    c = con.cursor()
+
     # 1. What are the 3 most popular articles.
     popular_article()
+
+    # Close the Database
+    con.close()
