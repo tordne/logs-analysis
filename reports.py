f983a0de36fe08130711e9553df085547fadaf1e
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2017-11-29 23:59:54 +0000

    Feat: Completed the first question to print the 3 top articles called popular_article

--- reports.py
+++ reports.py
@@ -7,9 +7,41 @@ def popular_article():
     '''
     Connect to the news database and
     extract the 3 most popular articles.
-    '''
 
-    pass
+    The WITH query makes 2 lists
+        * slug_list
+        * log_list
+
+    slug_list:
+        * Takes the title and slug from all the articles
+        * Modifies the slug and add a % in front of the slug
+
+    log_list:
+        * Takes the path and creates a num column
+        * It groups all the same path together and counts the total of each
+
+    The main SELECT:
+        * Outputs the title and num
+        * It compares both log_list and slug_list
+        * It will look in all the paths and will only return the ones which compare to path_q
+        * The list will be ordered with the most views on top and limited to the first 3
+    '''
+    c.execute(
+        "WITH slug_list AS ( \
+        SELECT title, '%' || slug AS path_q \
+        FROM articles \
+        ), log_list AS ( \
+        SELECT path, count(*) AS num \
+        FROM log\
+        GROUP BY path) \
+        SELECT title, num \
+        FROM slug_list, log_list \
+        WHERE path LIKE path_q \
+        ORDER BY num DESC \
+        LIMIT 3"
+    )
+    a = c.fetchall()
+    return a
 
 
 if __name__ == '__main__':
@@ -19,7 +51,8 @@ if __name__ == '__main__':
     c = con.cursor()
 
     # 1. What are the 3 most popular articles.
-    popular_article()
+    for title, views in popular_article():
+        print("{} -- {} views".format(title, views))
 
     # Close the Database
     con.close()
